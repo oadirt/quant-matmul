24023fdfb22369c865b104a81d10ff351a7772c1
Tri Dao <tridpq@gmail.com>
2024-01-27 22:11:48 -0800

    Bump to v1.1.1

--- quant_matmul/__init__.py
+++ quant_matmul/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.0.post1"
+__version__ = "1.1.1"
 
 from quant_matmul.quant_matmul_interface import preprocess_weight, quant_matmul_fn
 from quant_matmul.moe_interface import moe_mlp
